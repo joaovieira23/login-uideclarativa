5c56b1d3cdc55e672f4d1be6a75ab0af088fcc05
João Andrade <joaovictorvieira.23@hotmail.com>
2022-07-11 17:23:20 -0300

    chore: set up chakra theme

---
+++ styles/theme.ts
@@ -0,0 +1,40 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+    colors: {
+        gray: {
+            "900": "#181B23",
+            "800": "#1F2029",
+            "700": "#353646",
+            "600": "#4B4D63",
+            "500": "#616480",
+            "400": "#797D9A",
+            "300": "#9699B0",
+            "200": "#B3B5C6",
+            "100": "#D1D2DC",
+            "50": "#EEEEF2",
+        },
+        purple: {
+            "500": '#8257e5'
+        }
+    },
+    fontSizes: {
+        '6xl': '54px'
+    },
+    fonts: {
+        heading: 'Roboto',
+        body: 'Roboto',
+    },
+    radii: {
+        sm: '5px',
+        md: '8px'
+    },
+    styles: {
+        global: {
+            body: {
+                bg: 'gray.900',
+                color: 'gray.50'
+            }
+        }
+    }
+})
